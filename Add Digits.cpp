a092a4076f1cf27a8a317ea607b2c567233146eb
Arpita Bhatt <70204047+arpita938@users.noreply.github.com>
2022-02-08 13:22:54 +0530

    Create Add Digits.cpp

---
+++ Add Digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int addDigits(int num) {
+      int digitalRoot=0;
+      while(num>0){
+        digitalRoot=digitalRoot+num%10;
+        num/=10;
+        if(num==0 && digitalRoot>9){
+          num=digitalRoot;
+          digitalRoot=0;
+        }
+    }
+      return digitalRoot;
+};
